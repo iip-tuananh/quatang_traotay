546dfc281a4437f83507ac40b61ed70df54e65a8
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-25 15:56:42 +0700

    up

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,7 +21,7 @@
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script>
-      window.__ENV__ = window.__ENV || {}; __ENV__.link ='https://quatangtrungthanh.com/';
+      window.__ENV__ = window.__ENV || {}; __ENV__.link ='https://quatangcna.vn/';
       window.Laravel = {!! 
           json_encode([
               'csrf_token' => csrf_token(),
