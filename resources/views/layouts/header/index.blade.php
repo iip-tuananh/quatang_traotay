d41c02f6e5e95ed43adc885223ac0fae19542da5
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-28 09:21:02 +0700

    up

--- resources/views/layouts/header/index.blade.php
+++ resources/views/layouts/header/index.blade.php
@@ -19,6 +19,7 @@
                         <a  title="{{$setting->phone1}}" href="tel:{{$setting->phone1}}"> {{$setting->phone1}}</a>
                         </span>
                      </li>
+                     &nbsp;&nbsp;&nbsp;
                      @if($setting->phone2 != '')
                      <li>
                         Hotline 2: 
