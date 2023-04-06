1fc2cfa130c493f624ecff37a075432c02fd474f
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-04-06 16:16:35 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -383,7 +383,9 @@
 <section class="awe-section-10" id="awe-section-10">
    <div class="section section-brand mb-5">
       <div class="container">
-         <h2 class="hidden">Thương hiệu</h2>
+         <div class="section-title a-center">
+            <h2><span>Đối tác khách hàng</span></h2>
+         </div>
          <div class="slickbrand" data-lg-items="6" data-md-items="5" data-sm-items="4" data-xs-items="3" data-xss-items="2" data-margin="30">
             @foreach ($partner as $item)
             <div class="item">
